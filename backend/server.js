67df2eeb7800bcdc95c95c0b7696786de36992cd
Aaron Kurian <aaronkurianabraham@gmail.com>
2025-06-30 17:56:11 +0530

    Completed Structure

---
+++ backend/server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const cors = require("cors");
+const errorHandler = require("./middleware/errorHandler");
+const connectDb = require("./config/dbConnection");
+const dotenv = require("dotenv").config();
+
+connectDb();
+const app = express();
+
+const port = process.env.PORT || 5001;
+
+// CORS configuration
+app.use(cors({
+    origin: [
+        "http://localhost:5173", 
+        "http://127.0.0.1:5173",
+        "http://192.168.11.213:5173"
+    ],
+    credentials: true,
+    optionsSuccessStatus: 200,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"]
+}));
+
+app.use(express.json());
+app.use("/", (req, res, next) => {
+    console.log(`Request Method: ${req.method}, Request URL: ${req.url}`);
+    next();
+});
+app.use("/api/contacts", require("./routes/contactRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
+
+app.use(errorHandler);
+
+app.listen(port, () => {
+    console.log(`server running on port ${port}`);
+});
